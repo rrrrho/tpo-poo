d150fb72da24b6cf7d7395c99ba14f614aa4c186
pepi <fedeabeledo01@gmail.com>
2023-12-02 20:45:42 -0300

    agrego getters a ExoplanetaResponse

--- src/main/java/com/poo/trilateracion/dto/ExoplanetaResponse.java
+++ src/main/java/com/poo/trilateracion/dto/ExoplanetaResponse.java
@@ -1,8 +1,11 @@
 package com.poo.trilateracion.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ExoplanetaResponse {
     private static int currentId = 1;
-    String nombre;
+    private String nombre;
     double radio;
     double masa;
     public ExoplanetaResponse(String nombre, double radio, double masa) {
